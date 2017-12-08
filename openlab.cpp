0e20017667e74b16e3424654a4934fdedaf13f9d
Unknown <jpardo-cano729@west-mec.org>
2017-12-07 16:34:45 -0800

    moved over the cpp file

---
+++ openlab.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  int x;
+  int y;
+  int name;
+  std::cout << "Enter a value for the 'X' value" << std::endl;
+  std::cout << "X: ";
+  std::cin >> x;
+  std::cout << "Enter a value for the 'Y' value" << std::ednl;
+  std::cout << "Y: "
+  std::cout << "Now enter your name: "
+  std::cin >> name;
+  std::cout << "The information you entered"
+}
